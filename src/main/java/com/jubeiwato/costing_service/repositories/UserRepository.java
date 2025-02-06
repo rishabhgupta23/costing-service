93721c87daba653a1e8d7da66fd48426b22bce9f
Anjali Prasad <anjaliprasadanshu@gmail.com>
2025-02-06 11:46:40 +0530

    bom details update

--- src/main/java/com/jubeiwato/costing_service/repositories/UserRepository.java
+++ src/main/java/com/jubeiwato/costing_service/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.jubeiwato.costing_service.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jubeiwato.costing_service.entities.User;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailId(String emailId);
+
 }
