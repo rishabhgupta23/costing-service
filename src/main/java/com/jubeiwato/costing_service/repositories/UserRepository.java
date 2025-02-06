5c446e112541b13811d5c49fa2e25646887948ec
Anjali Prasad <anjaliprasadanshu@gmail.com>
2025-02-06 12:04:42 +0530

    previous application

--- src/main/java/com/jubeiwato/costing_service/repositories/UserRepository.java
+++ src/main/java/com/jubeiwato/costing_service/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package com.jubeiwato.costing_service.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jubeiwato.costing_service.entities.User;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
