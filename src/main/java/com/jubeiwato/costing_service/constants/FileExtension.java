6bd91d514275f580594bd9f1afb5ad1f2276c620
Anjali Prasad <anjaliprasadanshu@gmail.com>
2025-03-05 16:10:06 +0530

    vendorlist excel export

--- src/main/java/com/jubeiwato/costing_service/constants/FileExtension.java
+++ src/main/java/com/jubeiwato/costing_service/constants/FileExtension.java
@@ -1,9 +1,9 @@
 package com.jubeiwato.costing_service.constants;
 
-public enum SpreadsheetExtention {
-    xlsx("xlsx");
+public enum FileExtension {
+    SPREADSHEET("xlsx");
 
     private final String value;
-    SpreadsheetExtention(String value) { this.value = value; }
+    FileExtension(String value) { this.value = value; }
     public String getValue() { return value; }
 }
