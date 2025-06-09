8a4529c5db2f14750c9675b0ab201a8233201dc0
ANUSHA-001 <anusharaj8100@gmail.com>
2025-06-09 15:28:01 +0530

    awsS3Config changes

--- src/main/java/com/jubeiwato/costing_service/authentication/config/AwsS3Config.java
+++ src/main/java/com/jubeiwato/costing_service/authentication/config/AwsS3Config.java
@@ -3,7 +3,7 @@ package com.jubeiwato.costing_service.authentication.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -20,7 +20,7 @@ public class AwsS3Config {
 
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
 
     }
