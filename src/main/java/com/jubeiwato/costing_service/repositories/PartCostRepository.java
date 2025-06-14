3170d356fca2dc722dd39fb3e7efb28e9bcd7dab
Anjali Prasad <anjaliprasadanshu@gmail.com>
2025-06-14 18:46:01 +0530

    getRecentByPartId

--- src/main/java/com/jubeiwato/costing_service/repositories/PartCostRepository.java
+++ src/main/java/com/jubeiwato/costing_service/repositories/PartCostRepository.java
@@ -19,7 +19,7 @@ public interface PartCostRepository extends JpaRepository<PartCost, Long> {
             "    WHERE p2.part.id = :partId AND p2.vendor.id = p1.vendor.id" +
             ") AND p1.part.id = :partId")
 
-    List<PartCost> getByPartId(@Param("partId") Long partId);
+    List<PartCost> getRecentByPartId(@Param("partId") Long partId);
 
     @Query("SELECT pc FROM PartCost pc WHERE pc.part.partId = :partId AND pc.vendor.vendorId = :vendorId ORDER BY pc.updatedDateTime DESC")
     List<PartCost> fetchByPartIdAndVendorId(@Param("partId") Long partId, @Param("vendorId") Long vendorId);
