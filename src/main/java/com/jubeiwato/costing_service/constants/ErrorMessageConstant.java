a174f852ad43c2fae0052ab98019594001cf33a1
ANUSHA-001 <anusharaj8100@gmail.com>
2025-08-11 16:19:35 +0530

    bug fix

--- src/main/java/com/jubeiwato/costing_service/constants/ErrorMessageConstant.java
+++ src/main/java/com/jubeiwato/costing_service/constants/ErrorMessageConstant.java
@@ -76,6 +76,7 @@ public class ErrorMessageConstant {
     public static final String MISSING_AUTHORIZATION_HEADER= "Missing or invalid Authorization header";
     public static final String PASSWORD_RESET_NOT_REQUIRED= "Password reset not required";
     public static final String CYCLIC_DEPENDENCY_DETECTED= "Cyclic dependency detected: The child part already contains the parent in its BOM.";
+    public static final String PASSWORD_CANNOT_BE_CHANGED= "Cannot reset your own password";
 
     private ErrorMessageConstant() {
 
--- src/main/java/com/jubeiwato/validation/PasswordConstraintValidator.java
+++ src/main/java/com/jubeiwato/validation/PasswordConstraintValidator.java
@@ -8,7 +8,7 @@ import jakarta.validation.ConstraintValidatorContext;
 public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
 
     private static final String PASSWORD_PATTERN =
-            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!.])(?=\\S+$).{8,}$";
     //Password must contain at least 1 digit, 1 lowercase, 1 uppercase, 1 special character and be 8+ characters long        
 
     private Pattern pattern = Pattern.compile(PASSWORD_PATTERN); //Pattern.compile(...) compiles the string regex into a Pattern object.Done only once for efficiency (static pattern reuse).
