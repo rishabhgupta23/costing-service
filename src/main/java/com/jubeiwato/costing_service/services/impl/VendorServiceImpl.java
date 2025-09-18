b1cd8f6c8aabc48838c349005fdc3ab3e2f49857
ANUSHA-001 <anusharaj8100@gmail.com>
2025-09-18 17:08:29 +0530

    bug fix

--- src/main/java/com/jubeiwato/costing_service/services/impl/VendorServiceImpl.java
+++ src/main/java/com/jubeiwato/costing_service/services/impl/VendorServiceImpl.java
@@ -72,12 +72,12 @@ public class VendorServiceImpl implements VendorService {
                         throw new AppException(ErrorMessageConstant.INVALID_INPUT, HttpStatus.BAD_REQUEST);
                 }
 
-                if (emailId == null || !ValidationUtil.isValidEmail(emailId)) {
-                        throw new AppException("Invalid email format", HttpStatus.BAD_REQUEST);
-                    }
-                    
+                if (emailId!= null && !emailId.isBlank() && !ValidationUtil.isValidEmail(emailId)) {
+                throw new AppException(ErrorMessageConstant.INVALID_EMAIL_FORMAT, HttpStatus.BAD_REQUEST);
+                }
+   
                 Company company = companyRepository.findById(companyId)
-                                .orElseThrow(() -> new AppException("Company not found", HttpStatus.BAD_REQUEST));
+                                .orElseThrow(() -> new AppException(ErrorMessageConstant.COMPANY_DOES_NOT_EXIST, HttpStatus.BAD_REQUEST));
                 validateDuplicateVendor(companyId, vendorName);
                 Vendor vendor = Vendor.builder()
 
