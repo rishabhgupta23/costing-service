5be21c3c0e028bf54b11edf1a0cc37d361c1d666
AnjaliPrasad3 <anjaliprasadanshu@gmail.com>
2026-09-22 12:54:19 +0530

    bug fix

--- src/main/java/com/jubeiwato/costing_service/entities/CostFactor.java
+++ src/main/java/com/jubeiwato/costing_service/entities/CostFactor.java
@@ -2,12 +2,10 @@ package com.jubeiwato.costing_service.entities;
 
 import com.jubeiwato.costing_service.constants.CostFactorType;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
